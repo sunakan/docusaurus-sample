fbaebbc1b692ee79287cc9a665797ba1f2c8aeca
sunakan <sunakan03@gmail.com>
2021-08-27 16:35:18 +0900

    post

---
+++ 02-getting-started/hogehoge/blog/2021-02-28-greetings.md
@@ -0,0 +1,12 @@
+---
+slug: 挨拶
+title: 挨拶
+authors:
+  - name: sunakan
+    title: たいとる(著者の？)
+    url: https://github.com/sunakan
+    image_url: https://github.com/sunakan.png
+tags: [greetings]
+---
+
+わぁーい
--- 02-getting-started/hogehoge/docusaurus.config.js
+++ 02-getting-started/hogehoge/docusaurus.config.js
@@ -47,23 +47,6 @@ module.exports = {
           ],
         },
         {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
           title: 'More',
           items: [
             {
